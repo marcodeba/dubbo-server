5015749b0cc039c906c3ed01eefa9d13ea9c3594
panning255 <panning255@jk.cn>
2018-12-20 00:08:10 +0800

    Initial commit

---
+++ server-provider/src/main/java/com/gupaoedu/dubbo/Bootstrap.java
@@ -0,0 +1,17 @@
+package com.gupaoedu.dubbo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class Bootstrap {
+
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext
+                        ("META-INF/spring/dubbo-server.xml");
+        context.start();
+
+        System.in.read(); //阻塞当前进程
+    }
+}
