8f313e89dd33c882eb66f7892bce2fa9b2b41901
panning255 <panning255@jk.cn>
2020-03-10 11:25:55 +0800

    commit

--- server-provider/src/main/java/com/gupaoedu/dubbo/GpHelloImpl2.java
+++ server-provider/src/main/java/com/gupaoedu/dubbo/GpHelloImpl2.java
@@ -5,6 +5,6 @@ import com.alibaba.dubbo.rpc.RpcContext;
 public class GpHelloImpl2 implements IGpHello {
     @Override
     public String sayHello(String msg) {
-        return "Hello version 2 " + msg + "Request from consumer :" + RpcContext.getContext().getRemoteAddress();
+        return "Hello version 2 " + msg + ", Request from consumer :" + RpcContext.getContext().getRemoteAddress();
     }
 }
